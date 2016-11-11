84f4e8a6fc8438e93ca29f1377a8c0859f2aef48
alibhatti99 <alibhatti99@gmail.com>
2016-11-10 20:56:58 -0800

    Routing added

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,5 +4,8 @@ var jwt = require('jwt-simple');
 
 // export entire function to server.js
 module.exports = function(app) {
-  // define routes below
+  app.get('/api/test', function(req, res) {
+    res.json('yeee boiii it works!');
+    console.log('server side got hit up!')
+  })
 };
